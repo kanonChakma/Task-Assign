5afd0119641e33387bcd8d1da51a865356fb9d2a
kanon chakma <kanonchakma@pop-os.localdomain>
2022-12-01 18:36:23 +0600

    feat: add todo list

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Todo } from "../../utils";
+import InputField from "../Input/InputField";
+import TodoList from "../TodoList/TodoList";
 import "./Home.css";
 
 const Home: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [CompletedTodos, setCompletedTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), isDone: false, todo }]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="home">
       <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        CompletedTodos={CompletedTodos}
+        setCompletedTodos={setCompletedTodos}
+      />
     </div>
   );
 };
