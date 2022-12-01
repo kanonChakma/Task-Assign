010842d85f802aa3d8d868dc29a5646581af1731
kanon chakma <kanonchakma@pop-os.localdomain>
2022-12-01 21:00:04 +0600

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./component/Home/Home";
-import Login from "./component/Login/Login";
-import Register from "./component/Register/Register";
+import React, { useState } from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import "./App.css";
+import { todo } from "./common/model";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-  ]);
-  return <RouterProvider router={router} />;
+  const [toDo, setTodo] = useState<string>("");
+  const [toDoData, setToDoData] = useState<todo[]>([]);
+  const [completedTodo, setCompletedTodo] = useState<todo[]>([]);
+  const [progressTodo, setProgressTodo] = useState<todo[]>([]);
+
+  const handleToDoData = (e: React.FormEvent<EventTarget>): void => {
+    e.preventDefault();
+    if (toDo) {
+      setToDoData([...toDoData, { id: Date.now(), isDone: false, data: toDo }]);
+      setTodo("");
+    }
+  };
+
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+    let add,
+      active = toDoData,
+      complete = completedTodo,
+      progress = progressTodo;
+
+    if (source.droppableId === "activeTodos") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else if (source.droppableId === "progressTodos") {
+      add = progress[source.index];
+      progress.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+    if (destination.droppableId === "activeTodos") {
+      active.splice(destination.index, 0, add);
+    } else if (destination.droppableId === "progressTodos") {
+      progress.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+    setProgressTodo(progress);
+    setCompletedTodo(complete);
+    setToDoData(active);
+  };
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <span className="heading">Task Assign</span>
+        <InputField
+          toDo={toDo}
+          setToDo={setTodo}
+          handleToDoData={handleToDoData}
+        />
+
+        <TodoList
+          completedTodo={completedTodo}
+          toDoData={toDoData}
+          setCompletedTodo={setCompletedTodo}
+          setToDoData={setToDoData}
+          progressTodo={progressTodo}
+          setProgressTodo={setProgressTodo}
+        />
+      </div>
+    </DragDropContext>
+  );
 };
 
 export default App;
---
+++ src/components/ShowTodo.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Draggable } from "react-beautiful-dnd";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import { todo } from "../common/model";
+import "./styles.css";
+
+interface props {
+  index: number;
+  todos: todo;
+  toDoData: todo[];
+  setToDoData: React.Dispatch<React.SetStateAction<todo[]>>;
+}
+
+const ShowTodo: React.FC<props> = ({ todos, toDoData, setToDoData, index }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editData, setEditData] = useState<string>(todos.data);
+
+  const handleDone = (id: number) => {
+    setToDoData(
+      toDoData.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setToDoData(toDoData.filter((todo) => todo.id !== id));
+  };
+  const handleSubmit = (e: React.FormEvent<EventTarget>, id: number) => {
+    e.preventDefault();
+    setToDoData(
+      toDoData.map((todo) =>
+        todo.id === id ? { ...todo, data: editData } : todo
+      )
+    );
+    setEdit(false);
+  };
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+  return (
+    <Draggable draggableId={todos.id.toString()} index={index}>
+      {(provided, snapshot) => (
+        <form
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          className={`todos__single ${snapshot.isDragging ? "drag" : ""}`}
+          onSubmit={(e) => handleSubmit(e, todos.id)}
+        >
+          {edit ? (
+            <input
+              ref={inputRef}
+              className="todos__single--text"
+              value={editData}
+              onChange={(e) => setEditData(e.target.value)}
+            />
+          ) : todos.isDone ? (
+            <s className="todos__single--text">{todos.data}</s>
+          ) : (
+            <span className="todos__single--text">{todos.data}</span>
+          )}
+          <div>
+            <span
+              className="icon"
+              onClick={() => {
+                if (!edit && !todos.isDone) {
+                  setEdit(!edit);
+                }
+              }}
+            >
+              <AiFillEdit />
+            </span>
+            <span className="icon" onClick={() => handleDelete(todos.id)}>
+              <AiFillDelete />
+            </span>
+            <span className="icon" onClick={() => handleDone(todos.id)}>
+              <MdDone />
+            </span>
+          </div>
+        </form>
+      )}
+    </Draggable>
+  );
+};
+
+export default ShowTodo;
